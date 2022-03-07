86b9bf9448671cdba9d824cdd8c9e2b7fad19484
squaretron <squaretrontech@gmail.com>
2022-03-07 09:39:16 +0530

    Update notepage.js

--- src/pages/NotePage.js
+++ src/pages/NotePage.js
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from "react";
-// import { useState, useEffect } from "react";
 import { Link, useParams, useNavigate } from "react-router-dom";
 import { ReactComponent as ArrowLeft } from "../assets/arrow-left.svg";
 
